cc128c2e4fdf66c159ff24c1d74c14c62180d77a
guisantosalves <workguisantos@gmail.com>
2022-06-20 21:54:04 -0400

    inserting react router in the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+/*
+
+  Three routes
+  localhost.com/
+  localhost.com/checkout/
+  localhost.com/login/
+
+*/
 
 function App() {
   return (
     <div className="App">
-      <h1>amazon clone !</h1>
+      <Routes>
+          <Route path='/' element={<h1>amazon clone !</h1>} />
+          <Route path='/checkout' element={<h1>Checkout page</h1>}/>
+          <Route path='/login' element={<h1>Login page</h1>}/>
+      </Routes>
+      
     </div>
   );
 }
