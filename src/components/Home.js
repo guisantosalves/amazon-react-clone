7354273a659f8bb3f0ffebd7fcd40e246ec944be
guisantosalves <workguisantos@gmail.com>
2022-06-22 13:58:04 -0400

    banner is done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from "react";
-import "./css/Home.css"
+import "./css/Home.css";
 
-function Home(){
-    return(
-        <div>
-            <h1>I am in the home</h1>
-        </div>
-    )
+function Home() {
+  return (
+    <div className="home">
+      <img
+        className="home__image"
+        src="https://m.media-amazon.com/images/I/71+175b6EkL._SX3000_.jpg"
+        alt="amazon"
+      />
+    </div>
+  );
 }
 
 export default Home;
