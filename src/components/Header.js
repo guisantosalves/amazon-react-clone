5243f1022b798f5eb143880e8efb6d5b087bf7b1
guisantosalves <workguisantos@gmail.com>
2022-06-22 10:56:51 -0400

    doing the navbar and its style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,44 @@
 import React from "react";
-import './css/Header.css';
+import "./css/Header.css";
+import { Search } from "@mui/icons-material";
 
 //using react router
 import { Link } from "react-router-dom";
 
+//when we use href the page will refresh
 function Header() {
-    return (
-        <nav className="header">
+  return (
+    <nav className="header">
+      {/* logo -> img */}
+      <Link to="/login">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="amazon logo"
+        />
+      </Link>
 
-            {/* logo -> img */}
-            <Link to="/login">
-                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" />
-            </Link>
+      {/* search bar */}
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <Search className="header__searchIcon" />
+      </div>
 
-            {/* search bar */}
-            <input type="text" className="header__searchInput"/>
+      {/* 3 links */}
+      <div className="header__nav">
+        {/* 1 link */}
+        <Link>
 
-            {/* 3 links */}
-            {/* basket icon with number */}
-        </nav>
-    )
+        </Link>
+
+        {/* 2 link */}
+        {/* 3 link */}
+        {/* 4 link */}
+      </div>
+
+      {/* basket icon with number */}
+    </nav>
+  );
 }
 
 export default Header;
