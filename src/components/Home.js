80bd6c2dfee6a7b8443e425a96f915c34493cab3
guisantosalves <workguisantos@gmail.com>
2022-06-24 10:03:12 -0400

    putting exemple names in products

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ function Home() {
         />
         <Product
           id="2131312"
-          tittle="mouse"
+          tittle="Echo Dot (4ª Geração): Smart Speaker com Alexa - Cor Preta"
           price={11.22}
           rating={5}
           image="https://m.media-amazon.com/images/I/51nwegA8VdL._AC_SY450_.jpg"
@@ -32,21 +32,21 @@ function Home() {
       <div className="home__row">
         <Product
           id="2131312"
-          tittle="mouse"
+          tittle={`Novo Echo Show 5 (2ª Geração, versão 2021): Smart Display de 5" com Alexa e câmera de 2 MP - Cor Preta`}
           price={11.22}
           rating={5}
           image="https://m.media-amazon.com/images/I/51nwegA8VdL._AC_SY450_.jpg"
         />
         <Product
           id="2131312"
-          tittle="mouse"
+          tittle="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"
           price={11.22}
           rating={5}
           image="https://m.media-amazon.com/images/I/51nwegA8VdL._AC_SY450_.jpg"
         />
         <Product
           id="2131312"
-          tittle="mouse"
+          tittle="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever"
           price={11.22}
           rating={5}
           image="https://m.media-amazon.com/images/I/51nwegA8VdL._AC_SY450_.jpg"
@@ -56,7 +56,7 @@ function Home() {
       <div className="home__row exemplo">
       <Product
           id="2131312"
-          tittle="mouse"
+          tittle="moLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text everuse"
           price={11.22}
           rating={5}
           image="https://m.media-amazon.com/images/I/51nwegA8VdL._AC_SY450_.jpg"
